ef56f4bc7a09104e2dc107f6cc31b5a913895318
Alush Pranav <alushpranav@gmail.com>
2022-09-08 23:12:51 +0530

    added bubble sort

---
+++ sort/bubble.dart
@@ -0,0 +1,19 @@
+void sort(List<int> arr) {
+  int count = 0;
+  for (int i = arr.length; i > 0; i--) {
+    for (int j = 1; j < i; j++) {
+      if (arr[j - 1] > arr[j]) {
+        int temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+      }
+    }
+  }
+}
+
+void main() {
+  List<int> arr = [3, 2, 5, 4, 6, 1];
+  print('unsorted -> ' + arr.toString());
+  sort(arr);
+  print('bubble sorted -> ' + arr.toString());
+}
